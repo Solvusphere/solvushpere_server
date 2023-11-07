caf5055494bfe079ee2f164938fa73bc8c51ce6c
adilshaa <adilsha042@gmail.com>
2023-11-08 00:13:19 +0530

    changes

--- controllers/company/dashoard.controller.js
+++ controllers/company/dashoard.controller.js
@@ -1,6 +1,5 @@
 const Company = require("../../models/compaies.model");
 const Joi = require("joi");
-const bcrypt = require("bcrypt");
 const { commonErrors } = require("../../middlewares/error/commen.error");
 const User = require("../../models/users.model");
 const { sendEmail } = require("../../auth/email/nodemailer.auth");
@@ -13,10 +12,9 @@ const {
   redisGet,
 } = require("../../connections/redis.connection");
 const { verifyOtp } = require("../../auth/email/otp.auth");
-const Jwt = require('jsonwebtoken')
-const Redis = require("../../connections/redis.connection")
-const { hashPassword, campare } = require('../../utils/bcrypt')
-const { validateOtp, Validate } = require('../../validations/company.validation')
+const Jwt = require("jsonwebtoken");
+const { hashPassword, campare } = require("../../utils/bcrypt");
+const { Validate } = require("../../validations/company.validation");
 
 const requirments = {
   password: Joi.string().min(8).required(),
@@ -176,37 +174,47 @@ const CompanyController = {
     }
   },
 
-    async login(req, res) {
-        try {
-            const { email, password } = req.body
-            const CompanyData = {
-             email: email,
-             password: password,
-            };
-            const validating = Validate(
-                { email: requirments.email, password: requirments.password },
-                CompanyData
-            )
-            if (!validating)
-                return commonErrors(res, 400, validating.response[0].message)
-            
-            const company = await Company.findOne({ email: email })
-            if (!company)
-                return commonErrors(res, 404, { message: "Please Register Your Company" })
-            
-            const isValidPassword = await campare(password, company.password)
-            
-            if (!isValidPassword)
-                return commonErrors(res, 400, { message: "Password Doesn't Match" })
-            
-            const payload = { _id: company._id, name: company.name, email: company.email };
-            const token = Jwt.sign(payload, "#$solvusphere$#")
-            return commonErrors(res, 200, { message: "Login Successfully", token, company });
-        } catch (error) {
-            console.log(error);
-            return commonErrors(error,500,{message:"Internal Server Error"})
-        }
-    }
-}
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const CompanyData = {
+        email: email,
+        password: password,
+      };
+      const validating = Validate(
+        { email: requirments.email, password: requirments.password },
+        CompanyData
+      );
+      if (!validating)
+        return commonErrors(res, 400, validating.response[0].message);
 
-module.exports = CompanyController
+      const company = await Company.findOne({ email: email });
+      if (!company)
+        return commonErrors(res, 404, {
+          message: "Please Register Your Company",
+        });
+
+      const isValidPassword = await campare(password, company.password);
+
+      if (!isValidPassword)
+        return commonErrors(res, 400, { message: "Password Doesn't Match" });
+
+      const payload = {
+        _id: company._id,
+        name: company.name,
+        email: company.email,
+      };
+      const token = Jwt.sign(payload, "#$solvusphere$#");
+      return commonErrors(res, 200, {
+        message: "Login Successfully",
+        token,
+        company,
+      });
+    } catch (error) {
+      console.log(error);
+      return commonErrors(error, 500, { message: "Internal Server Error" });
+    }
+  },
+};
+
+module.exports = CompanyController;
