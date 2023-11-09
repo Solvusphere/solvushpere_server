314829d711f74f0540fac786a6d5ac71240f6b15
adilshaa <adilsha042@gmail.com>
2023-11-09 20:43:59 +0530

    temprorly commiting

--- utils/process.NLP.js
+++ utils/process.NLP.js
@@ -5,10 +5,10 @@ const TfIdf = natural.TfIdf;
 
 module.exports.processingResult(async (id) => {
   const tfidf = new TfIdf();
-
+  
   let fetchingSolutions = await Goals.find({ company_id: id }).exec();
   if (!fetchingSolutions) {
     return false;
-    }
-    console.log(fetchingSolutions);
+  }
+  console.log(fetchingSolutions);
 });
